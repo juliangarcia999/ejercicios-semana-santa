89452291a1e9ca7f064ca832bef494f5c55b748c
julia <julia@DESKTOP-J5GC7S4>
2019-04-22 20:30:16 +0200

    Ejercicio java

---
+++ src/ej/pkg10/tema/pkg7/Ej10Tema7.java
@@ -0,0 +1,20 @@
+package ej.pkg10.tema.pkg7;
+import java.util.Scanner;
+public class Ej10Tema7 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        for (int i = 0; i < 20; i++) {
+            int numerorandom = (int)(Math.random()*10); 
+            System.out.println(numerorandom);
+            if (numerorandom%2==0){
+            int aux = 0;
+            numerorandom = 0;
+            aux = numerorandom;
+            
+            }
+        }
+    }
+    
+}
